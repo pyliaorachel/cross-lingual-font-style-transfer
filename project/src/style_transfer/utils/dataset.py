ac77602512366f9b3738178346ddc82b616802f1
Peiyu Liao <pyliao@stanford.edu>
2019-06-01 02:05:01 -0700

    Add dtype

--- project/src/style_transfer/utils/dataset.py
+++ project/src/style_transfer/utils/dataset.py
@@ -7,9 +7,11 @@ from .utils import image_loader, is_image_file
 
 
 class Dataset(data.Dataset):
-    def __init__(self, dataset, imsize):
+    def __init__(self, dataset, imsize, dtype=torch.FloatTensor):
         self.dataset = dataset
         self.imsize = imsize
+        self.dtype = dtype
+
         self.list_IDs = [os.path.join(dataset, fname) for fname in os.listdir(dataset) if is_image_file(os.path.join(dataset, fname))]
 
     def __len__(self):
@@ -17,5 +19,5 @@ class Dataset(data.Dataset):
 
     def __getitem__(self, index):
         image_name = self.list_IDs[index]
-        X = image_loader(image_name, self.imsize, first_dim=False)
+        X = image_loader(image_name, self.imsize, first_dim=False).type(self.dtype)
         return X, image_name
