e4e3536309cf23f4bfe6f0b41cd92033785de844
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-02 05:15:50 +0000

    testing script

--- data/genFont.py
+++ data/genFont.py
@@ -6,4 +6,8 @@ for name in F:
     filename = "pingfang/" + name + ".png"
     # print name
     F[name].export(filename, 128)
+
+    # x = F[21121]
+    # x = F["Identity.21121"]
+    # y = x.unicode # y = 29508
     # F[name].export(filename, 600)     # set height to 600 pixels
